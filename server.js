9c6fb3eeb09ee381212554e8bd5f174b61c495d0
Christopher Chu <cchu@zipcar.com>
2016-04-08 19:28:24 -0400

    made a /www for web content

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/www'));
 
 app.listen(1234, function () {
   console.log('Web client running on port 1234');
