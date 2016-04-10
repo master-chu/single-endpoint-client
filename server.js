b3064ed45a916eb02b989d0504231fce4985d34e
Master Chu <e1337chu@gmail.com>
2016-04-10 10:51:47 -0400

    COLORZ!!!!1

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var colors = require('colors');
 
 var app = express();
 app.use(express.static(__dirname + '/www'));
 
 app.listen(1234, function () {
-  console.log('Web client running on port 1234');
+  console.log(colors.green('Web client running on port 1234'));
 });
