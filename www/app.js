1a55c466b293acd5845ff6716b1aacbabf19023f
Christopher Chu <cchu@zipcar.com>
2016-04-13 11:52:12 -0400

    flexible param added

--- www/app.js
+++ www/app.js
@@ -1,80 +1,80 @@
 $(function(){
-  var LOCATIONS_FIXTURE = [
-    {
-      "address": "370 Congress St",
-      "description": "370 Congress St - Residence Inn",
-      "distance": "0.096931432218279557589414352483819570814",
-      "distance_unit": "mi",
-      "latitude": "1234",
-      "longitude": "1234",
-      "location_id": 1706002427,
-      "vehicles": [
-        {
-          "image_url": "http://media.zipcar.com/images/model-image?model_id=1298956229&mode=med",
-          "location_id": 1706002427,
-          "make": "Honda",
-          "model": "Fit",
-          "name": "Micah",
-          "currency_html_entity": "$",
-          "daily_cost_max": null,
-          "daily_cost_min": null,
-          "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
-          "hourly_cost_max": 3.81,
-          "hourly_cost_min": 3.5,
-          "location_id": 81343210,
-          "model_id": 1298956229,
-          "pool_id": 81568710,
-          "unlimited": true,
-          "vehicle_id": 1621700612
-        }
-      ]
-    }, {
-      "address": "2 Center Plaza",
-      "description": "2 Center Plaza",
-      "distance": "0.8008545054",
-      "distance_unit": "mi",
-      "in_communications": true,
-      "latitude": 42.3595659,
-      "location_id": 81343210,
-      "longitude": -71.060189,
-      "vehicles": [
-        {
-          "currency_html_entity": "$",
-          "daily_cost_max": null,
-          "daily_cost_min": null,
-          "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
-          "hourly_cost_max": 3.81,
-          "hourly_cost_min": 3.5,
-          "image_url": "http://media.zipcar.com/images/model-image?model_id=1298956229&mode=med",
-          "location_id": 81343210,
-          "make": "BMW",
-          "model": "X1",
-          "model_id": 1298956229,
-          "name": "Bendt",
-          "pool_id": 81568710,
-          "unlimited": false,
-          "vehicle_id": 1621700612
-        },
-        {
-          "currency_html_entity": "$",
-          "daily_cost_max": null,
-          "daily_cost_min": null,
-          "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
-          "hourly_cost_max": 3.69,
-          "hourly_cost_min": 3.38,
-          "image_url": "http://media.zipcar.com/images/model-image?model_id=68438186&mode=med",
-          "location_id": 81343210,
-          "make": "Subaru",
-          "model": "Impreza AWD 5-door",
-          "model_id": 68438186,
-          "name": "Scott G",
-          "pool_id": 81568872,
-          "unlimited": false,
-          "vehicle_id": 1577476714
-        }
-      ]
-    }
-  ];
+  // var LOCATIONS_FIXTURE = [
+  //   {
+  //     "address": "370 Congress St",
+  //     "description": "370 Congress St - Residence Inn",
+  //     "distance": "0.096931432218279557589414352483819570814",
+  //     "distance_unit": "mi",
+  //     "latitude": "1234",
+  //     "longitude": "1234",
+  //     "location_id": 1706002427,
+  //     "vehicles": [
+  //       {
+  //         "image_url": "http://media.zipcar.com/images/model-image?model_id=1298956229&mode=med",
+  //         "location_id": 1706002427,
+  //         "make": "Honda",
+  //         "model": "Fit",
+  //         "name": "Micah",
+  //         "currency_html_entity": "$",
+  //         "daily_cost_max": null,
+  //         "daily_cost_min": null,
+  //         "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
+  //         "hourly_cost_max": 3.81,
+  //         "hourly_cost_min": 3.5,
+  //         "location_id": 81343210,
+  //         "model_id": 1298956229,
+  //         "pool_id": 81568710,
+  //         "unlimited": true,
+  //         "vehicle_id": 1621700612
+  //       }
+  //     ]
+  //   }, {
+  //     "address": "2 Center Plaza",
+  //     "description": "2 Center Plaza",
+  //     "distance": "0.8008545054",
+  //     "distance_unit": "mi",
+  //     "in_communications": true,
+  //     "latitude": 42.3595659,
+  //     "location_id": 81343210,
+  //     "longitude": -71.060189,
+  //     "vehicles": [
+  //       {
+  //         "currency_html_entity": "$",
+  //         "daily_cost_max": null,
+  //         "daily_cost_min": null,
+  //         "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
+  //         "hourly_cost_max": 3.81,
+  //         "hourly_cost_min": 3.5,
+  //         "image_url": "http://media.zipcar.com/images/model-image?model_id=1298956229&mode=med",
+  //         "location_id": 81343210,
+  //         "make": "BMW",
+  //         "model": "X1",
+  //         "model_id": 1298956229,
+  //         "name": "Bendt",
+  //         "pool_id": 81568710,
+  //         "unlimited": false,
+  //         "vehicle_id": 1621700612
+  //       },
+  //       {
+  //         "currency_html_entity": "$",
+  //         "daily_cost_max": null,
+  //         "daily_cost_min": null,
+  //         "dayAvailability": "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1",
+  //         "hourly_cost_max": 3.69,
+  //         "hourly_cost_min": 3.38,
+  //         "image_url": "http://media.zipcar.com/images/model-image?model_id=68438186&mode=med",
+  //         "location_id": 81343210,
+  //         "make": "Subaru",
+  //         "model": "Impreza AWD 5-door",
+  //         "model_id": 68438186,
+  //         "name": "Scott G",
+  //         "pool_id": 81568872,
+  //         "unlimited": false,
+  //         "vehicle_id": 1577476714
+  //       }
+  //     ]
+  //   }
+  // ];
 
   // var server = new Pretender(function() {
   //   this.get('http://localhost:8081/vehicles/nearby', function(request) {
@@ -93,22 +93,78 @@ $(function(){
     DEFAULT_END_TIME = '2016-04-20T13:00:00+0000',
     DEFAULT_ACCOUNT_ID = 1247133268,
     DEFAULT_DRIVER_ID = 1247131032,
+    DEFAULT_FLEXIBLE = false,
     DEFAULT_USER_ID = 1181079380;
+
+  var FlashMessages = React.createClass({
+    render: function() {
+      var messages = this.props.messages;
+      var clearButton;
+      if (messages.length > 0) {
+        clearButton =
+          <button onClick={this.props.onClearFlashMessages} className='btn btn-sm btn-primary'>
+            Clear
+          </button>;
+      }
+
+      var formattedMessages = messages.map(function(message) {
+        var className = message.className || 'warning';
+        return (
+          <div className={'bg-' + className + ' flash-message'}>
+            <span className={'text-' + className}>
+              {message.content}
+            </span>
+          </div>
+        );
+      });
+
+      return (
+        <div id='flash-messages'>
+          {formattedMessages}
+          {clearButton}
+        </div>
+      );
+    }
+  });
 
   var LocationSearch = React.createClass({
     getInitialState: function() {
-      return { locations: [] };
+      return {
+        locations: [],
+        flashMessages: []
+      };
     },
     render: function() {
       return (
-        <div className="row">
-          <LocationSearchForm onSearch={this.handleSearch} />
-          <LocationsTable locations={this.state.locations}/>
+        <div>
+          <FlashMessages
+            messages={this.state.flashMessages}
+            onClearFlashMessages={this.clearFlashMessages} />
+          <div className="row">
+            <LocationSearchForm
+              onSearch={this.handleSearch}
+              onPushFlashMessage={this.handlePushFlashMessage}
+            />
+            <LocationsTable locations={this.state.locations}/>
+          </div>
         </div>
       );
     },
     handleSearch: function(params) {
       this.loadLocationsFromServer(params);
+    },
+    handlePushFlashMessage: function(message) {
+      this.pushFlashMessage(message);
+    },
+    clearFlashMessages: function() {
+      this.setState({
+        flashMessages: []
+      });
+    },
+    pushFlashMessage: function(message) {
+      this.setState({
+        flashMessages: this.state.flashMessages.concat([message])
+      });
     },
     loadLocationsFromServer: function(params) {
       console.log('searching...');
@@ -124,7 +180,8 @@ $(function(){
           'start_time': params['startTime'],
           'end_time': params['endTime'],
           'account_id': params['accountId'],
-          'driver_id': params['driverId']
+          'driver_id': params['driverId'],
+          'flexible': params['flexible']
         },
         headers: {
           'Zc-User-Id': params['userId']
@@ -135,6 +192,10 @@ $(function(){
         }.bind(this),
         error: function(xhr, status, err) {
           console.error(this.props.url, status, err.toString());
+          this.pushFlashMessage({
+            content: [this.props.url, status, err.toString()].join(" "),
+            className: 'danger'
+          });
         }.bind(this)
       });
     }
@@ -164,6 +225,9 @@ $(function(){
     handleDriverIdChange: function(event) {
       this.setState({driverId: event.target.value});
     },
+    handleFlexibleChange: function(event) {
+      this.setState({flexible: event.target.checked});
+    },
     getInitialState: function() {
       return {
         latitude: DEFAULT_LATITUDE,
@@ -172,6 +236,7 @@ $(function(){
         endTime: DEFAULT_END_TIME,
         accountId: DEFAULT_ACCOUNT_ID,
         driverId: DEFAULT_DRIVER_ID,
+        flexible: DEFAULT_FLEXIBLE,
         userId: DEFAULT_USER_ID
       };
     },
@@ -206,7 +271,10 @@ $(function(){
       autocomplete.addListener('place_changed', function() {
         var place = autocomplete.getPlace();
         if (!place.geometry) {
-          console.error('Google Places Autocomplete could not find a location :(');
+          _this.pushFlashMessage({
+            content: 'Google Places Autocomplete could not find a location :(',
+            className: 'danger'
+          });
           return;
         } else {
           _this.setState({
@@ -216,17 +284,22 @@ $(function(){
         }
       });
     },
+    pushFlashMessage: function(message) {
+      this.props.onPushFlashMessage(message);
+    },
     search: function(event) {
       event.preventDefault();
       var _this = this;
 
-      var searchKeys = ['latitude', 'longitude', 'startTime', 'endTime', 'accountId', 'driverId'];
+      var requiredSearchKeys = ['latitude', 'longitude', 'startTime', 'endTime', 'accountId', 'driverId'];
+      var optionalSearchKeys = ['flexible'];
+
       var searchParams = {};
-      searchKeys.forEach(function(key) {
+      requiredSearchKeys.concat(optionalSearchKeys).forEach(function(key) {
         searchParams[key] = _this.state[key];
       });
 
-      if (searchKeys.some(function(key) { return !searchParams[key]; })) {
+      if (requiredSearchKeys.some(function(key) { return !searchParams[key]; })) {
         console.error('Missing at least 1 required search parameter');
         return;
       } else {
@@ -313,6 +386,16 @@ $(function(){
                 value={this.state.driverId}
                 onChange={this.handleDriverIdChange}
               />
+            </div>
+            <div class="checkbox">
+              <label>
+                <input
+                  type="checkbox"
+                  value={this.state.flexible}
+                  onChange={this.handleFlexibleChange}
+                />
+                &nbsp;my time is flexible
+              </label>
             </div>
             <input className="btn btn-primary" type="submit" value="Search" />
           </form>
